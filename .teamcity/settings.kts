b73ed5b413c6d2187125cff5d7c6a19ee426bda5
admin <admin@TeamCity>
2021-05-05 19:06:40 +1000

    TeamCity change in 'SmartRouter' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,6 +3,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetBuild
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetPack
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetPublish
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetTest
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 /*
@@ -63,6 +64,12 @@ object Build : BuildType({
             name = "Publish"
             projects = "SmartRouter.sln"
             param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
+        }
+        powerShell {
+            name = "batect"
+            scriptMode = script {
+                content = "./batect build"
+            }
         }
     }
 
