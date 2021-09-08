e8b1a0a5de4569d750e6c6ff2bbf8443aa30c3bb
Liangtian Wang <liangtian.wang@thoughtworks.com>
2021-09-08 15:52:38 +1000

    Update batect to use .Net5 to simplify the sample project

--- .batect/build-env-framework/install.ps1
+++
@@ -1,6 +0,0 @@
-Invoke-WebRequest 'https://github.com/git-for-windows/git/releases/download/v2.25.0.windows.1/MinGit-2.25.0-64-bit.zip' -OutFile c:\MinGit.zip
-Expand-Archive c:\MinGit.zip -DestinationPath c:\MinGit;
-$env:PATH = $env:PATH + ';C:\MinGit\cmd\;C:\MinGit\cmd';
-Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment\' -Name Path -Value $env:PATH
-
-dotnet tool install --global dotnet-reportgenerator-globaltool --version 4.3.1
